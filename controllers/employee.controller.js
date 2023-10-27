8ea63a361c42d1b4bac1829bae95367d3764ed7c
nasii05 <iamnasih@gmail.com>
2023-10-27 18:56:38 +0530

    404 middleware added🧨

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {validateDbId}  = require('../middlewares/index.js');
 
 const Employee = require('../models/employee.model.js');
 const { generateCrudMethods } = require('../services/index.js');
 const employeecrud = generateCrudMethods(Employee);
+const { validateDbId, raiseRecord404Error } = require('../middlewares/index.js');
+
 
 router.get('/test',
-    (req, res, next) => {next()},
-    (req, res) => {res.send('foo')}
+    (req, res, next) => { next({}) },
+    (req, res) => { res.send('foo') }
 )
 
 router.get('/', (req, res) => {
@@ -17,17 +18,15 @@ router.get('/', (req, res) => {
         .catch(err => console.log(err));
 });
 
-router.get('/:id', validateDbId,  (req, res) => {
-        employeecrud.getById(req.params.id)
-            .then(data => {
-                if (data)
-                    res.send(data)
-                else
-                    res.status(404).json({
-                        error: 'no recode with given _id' + req.params.id
-                    })
-            })
-            .catch(err => console.log(err));
+router.get('/:id', validateDbId, (req, res) => {
+    employeecrud.getById(req.params.id)
+        .then(data => {
+            if (data)
+                res.send(data)
+            else
+              raiseRecord404Error(req, res)
+        })
+        .catch(err => console.log(err));
 });
 
 router.post('/', (req, res) => {
@@ -36,11 +35,11 @@ router.post('/', (req, res) => {
         .catch(err => console.log(err));
 });
 
-router.put('/:id', (req, res) => {
-    
+router.put('/:id', validateDbId, (req, res) => {
+
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateDbId,(req, res) => {
 
 })
 
--- middlewares/index.js
+++ middlewares/index.js
@@ -4,12 +4,26 @@ const objectId = require('mongoose').Types.ObjectId;
 const validateDbId = (req, res, next) => {
     if (ObjectId.isValid(req.params.id) == false)
         res.status(400).json({
-            error: 'given object id is not valid'
+            error: `given object id (${req.params.id})is not valid.`
         })
     else
-    next()
+        next()
+}
+
+const raiseRecord404Error = (req, res) => {
+    res.status(404).json({
+        error: 'no recode with given _id' + req.params.id
+    })
+}
+
+const errorHandler = (error, req, res, next) => {
+    res.status(500).json({
+        error
+    })
 }
 
 module.exports = {
-    validateDbId
+    validateDbId,
+    raiseRecord404Error,
+    errorHandler
 }
